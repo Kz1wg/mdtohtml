2fad42317ca7dca15498a90b65a8325297dc76e8
KIwGucci <kiw.a.gucci@gmail.com>
2022-04-20 00:27:18 +0900

    added css style

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
-use pulldown_cmark::{html, Options, Parser};
+use maud::{html, Markup, PreEscaped, Render, DOCTYPE};
+use pulldown_cmark::{html::push_html, Options, Parser};
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
@@ -10,43 +11,69 @@ fn main() -> Result<()> {
     markdown_to_html(readfiles)?;
     Ok(())
 }
-
+fn getcss() -> String {
+    let mut cssfile = File::open("./mdcss.css").expect("can not open css file");
+    let mut css_content = String::new();
+    cssfile
+        .read_to_string(&mut css_content)
+        .expect("can not read css file");
+    css_content
+}
 fn markdown_to_html(targets: &[String]) -> Result<()> {
+    // markdownパーサで使用するオプションを指定
     let mut options = Options::empty();
     options.insert(Options::ENABLE_STRIKETHROUGH);
     options.insert(Options::ENABLE_TABLES);
     options.insert(Options::ENABLE_SMART_PUNCTUATION);
-
+    let mdcss = &getcss();
     for target in targets {
+        // markdownファイルを読み込み
         let targetfile = File::open(target)?;
         let mut reader = BufReader::new(targetfile);
         let mut markdownstr = String::new();
         reader.read_to_string(&mut markdownstr)?;
-        let parser = Parser::new_ext(&markdownstr, options);
 
-        // Write to String buffer.
-        let mut html_output = String::new();
-        html::push_html(&mut html_output, parser);
+        // htmlファイルの名前を生成
         let filename: Vec<&str> = target.split(".").collect();
         let filename = [filename[0], "html"].join(".");
-        let markuptext = markup(html_output);
+
+        // ヘッダを生成
+        let headtext = html! {
+            header{
+            meta charset="utf-8";
+            (html!{
+                style{
+                    (mdcss);
+                }
+            })
+            title { "Document" };
+            }
+        };
+
+        // markdownをhtmlに変換
+        let markuptext = html! {
+            (DOCTYPE)html{
+                (headtext)
+                (Markdown(&markdownstr).render())
+            }
+        };
+        // htmlをファイルに書き込み
         let mut writebuffer = File::create(filename)?;
-        writebuffer.write(markuptext.as_bytes())?;
+
+        writebuffer.write(markuptext.render().into_string().as_bytes())?;
     }
     Ok(())
 }
 
-fn markup(bodytext: String) -> String {
-    use maud::{html, DOCTYPE};
+/// Renders a block of Markdown using `pulldown-cmark`.
+struct Markdown<T: AsRef<str>>(T);
 
-    let markup = html! {
-        (DOCTYPE)
-        html {
-            head {
-                meta charset="utf-8";
-                title { "Markdown" }
-            }
-        }
-    };
-    markup.into_string() + &bodytext
+impl<T: AsRef<str>> Render for Markdown<T> {
+    fn render(&self) -> Markup {
+        // Generate raw HTML
+        let mut my_html = String::new();
+        let parser = Parser::new(self.0.as_ref());
+        push_html(&mut my_html, parser);
+        PreEscaped(my_html)
+    }
 }
